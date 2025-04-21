f485f730fb441f6ef05bc463372413982af237f6
Dominik Engel <git@d0me.xyz>
2025-04-21 09:36:26 +0300

    enable restart unless stopped and give name

--- run_camel.sh
+++ run_camel.sh
@@ -1,1 +1,1 @@
-docker run -d --gpus all --user 217835:1217835 -v ./Workspace:/workspace -p 192.168.192.3:8888:8888 camelgo
+docker run -d --gpus all --restart unless-stopped --name CamelGo --user 217835:1217835 -v ./Workspace:/workspace -p 192.168.192.3:8888:8888 camelgo
